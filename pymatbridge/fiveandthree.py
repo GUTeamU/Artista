770543253e3d0ab0cd1028b8a5521e70bbe8e791
fjleishman <fjleishman@gmail.com>
2014-02-25 14:56:06 +0000

    removing unnecessary test files

--- pymatbridge/fiveandthree.py
+++
@@ -1,8 +0,0 @@
-from pymatbridge import Matlab
-mlab = Matlab(matlab='/usr/local/MATLAB/R2013a/bin/matlab')
-mlab.start()
-res = mlab.run_func('/users/level3/1102103l/Desktop/pymatbridge/fiveandthree.m', {'arg1': 3, 'arg2': 5})
-print res['result']
-
-###http://stackoverflow.com/questions/2883189/calling-matlab-functions-from-python
-
--- pymatbridge/test.py
+++
@@ -1,6 +0,0 @@
-from pymatbridge import Matlab
-mlab = Matlab(matlab='/usr/local/MATLAB/R2013a/bin/matlab')
-mlab.start()
-res = mlab.run_func('/users/level3/1102103l/Desktop/pymatbridge/yourfunc.m', {'arg1': 3, 'arg2': 5})
-print res['result']
-
