6c30343dd68381ee025481dcab912d303b097819
Fraser Leishman <fjleishman@gmail.com>
2014-02-27 15:09:07 +0000

    Update lineDect.py

--- pymatbridge/lineDect.py
+++ pymatbridge/lineDect.py
@@ -1,5 +1,5 @@
 from pymatbridge import Matlab
 mlab = Matlab(matlab='/usr/local/MATLAB/R2013a/bin/matlab')
 mlab.start()
-res = mlab.run_func('/users/level3/1102103l/Desktop/pymatbridge/lineDect.m', {'arg1': '/users/level3/1102103l/Desktop/pymatbridge/fraser.jpg', 'arg2':'/users/level3/1102103l/Desktop/pymatbridge/saved.jpg'})
+res = mlab.run_func('./lineDect.m', {'arg1': './fraser.jpg', 'arg2':'./saved.jpg'})
 #print res['result']
