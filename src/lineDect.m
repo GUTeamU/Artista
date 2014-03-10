82d081fd526a6b6432839a86c5975cce10a93f0a
Team U <teamu@teamu-OptiPlex-740-Enhanced.(none)>
2014-03-10 14:53:30 +0000

    Merged pymatlab with dev

--- src/lineDect.m
+++ src/lineDect.m
@@ -1,34 +1,28 @@
 % Matlab Script to detect lines on an image
-clf
 
-% load in image
-original = imread ('fraser.jpg');
-figure('name', 'original image'), imshow(original)
+function image = lineDect(args)
+	% load in image
+	readimage = args.arg1
+	saveimage = args.arg2
+	original = imread (readimage);
 
-% convert to greyscale (notice spelling :( )
-grayscale = rgb2gray(original);
-figure('name', 'grayscale image'), imshow(grayscale)
+	% convert to greyscale (notice spelling :( )
+	grayscale = rgb2gray(original);
 
-% adjust contrast to darken the image, 
-contrast = imadjust(grayscale,[0.3 0.6], []);
-figure('name', 'darkened image'), imshow(contrast)
+	% adjust contrast to darken the image, 
+	contrast = imadjust(grayscale,[0.3 0.6], []);
 
-% create the gaussian filter with hsize = [5 5] and sigma = 2
-G = fspecial('gaussian',[13 13],3);
+	% create the gaussian filter with hsize = [5 5] and sigma = 2
+	G = fspecial('gaussian',[13 13],3);
 
-% filter it to blur the image, and display
-blurContrast = imfilter(contrast,G,'same');
-figure('name', 'darkened image with Gaussian blur filter'), imshow(blurContrast)
+	% filter it to blur the image, and display
+	blurContrast = imfilter(contrast,G,'same');
 
-% edge detection on blurred increased contrast image 
-cannyDetection = edge (blurContrast, 'canny', [0.15, 0.3]);
-figure('name', 'canny line detection on image'), imshow(cannyDetection)
+	% edge detection on blurred increased contrast image 
+	cannyDetection = edge (blurContrast, 'canny', [0.15, 0.3]);
 
-% wiener filter to gel lines together
-wiener = wiener2(cannyDetection, [3 3]);
-figure('name', 'wiener filter gels dots into ticker lines'), imshow(wiener)
-imwrite(wiener, '/users/level3/1102103l/University/matlab/wiener.jpg');
-
+	imwrite(cannyDetection, saveimage);
+end
 
 
 
